b9a0942c407ea6762c5221ba86fc351ff8588149
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-18 15:28:05 -0700

    Initial commit for Lab 9, starting with Arrays

---
+++ stdArray.cpp
@@ -0,0 +1,13 @@
+//COMSC-210 | Lab 9 (Part 1) | Eric-Giulio Hedes
+#include <iostream>
+#include <iomanip>
+#include <array>
+#include <fstream>
+using namespace std;
+
+const int SIZE = 30;
+
+int main()
+{
+    
+}
