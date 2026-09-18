e1ffb45cff8d7dd53b1e1fb78ccd8df169e04ed0
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-18 16:09:30 -0700

    Doing progress on arrays
    TODO: To fix the issue where the program completely stops when putting the numbers in the array itself

--- stdArray.cpp
+++ stdArray.cpp
@@ -3,11 +3,28 @@
 #include <iomanip>
 #include <array>
 #include <fstream>
+#include <string>
 using namespace std;
 
 const int SIZE = 30;
 
 int main()
 {
+    array<string, SIZE> heights;
+
+    ifstream file;
+    int line, i = 0;
+    file.open("test.txt");
+    if (!file.good()) throw "I/O error";
+    if (file.is_open())
+    {
+        while (cin >> line)
+        {
+            heights[i] = line;
+            i++;
+        }
+    }
     
+    file.close();
+    return 0;
 }
