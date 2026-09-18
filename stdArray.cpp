cbc663a0b105b05610a377ab80359cef0435f471
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-18 16:43:35 -0700

    Added other array functions

--- stdArray.cpp
+++ stdArray.cpp
@@ -4,6 +4,8 @@
 #include <array>
 #include <fstream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 //SIZE is the maximum size for the array, and in this case, it will be 30.
@@ -12,8 +14,11 @@ const int SIZE = 30;
 int main()
 {
     //Declare the array with the maximum size and the data type
-    //This array will most likely be based on human heights, averaging from 120 to 200 cm.
+    //This array will most likely be based on human adult heights, averaging from 120 to 200 cm.
     array<int, SIZE> heights;
+
+    //Random seed generator
+    srand(time(0));
 
     //Add a file variable that'll be used up for adding array elements
     ifstream file;
@@ -39,5 +44,16 @@ int main()
     cout << endl;
     //2) Array size
     cout << "2) Size: " << heights.size() << endl;
+    //3) 15th element
+    cout << "3) 15th element: " << heights[14] << endl;
+    //4) Random element
+    cout << "4) Random element: " << heights[rand() % (SIZE - 1)] << endl;
+    //5) Sum of all elements
+    int sum;
+    for (int i = 0; i < SIZE; i++)
+        sum += heights[i];
+    cout << "5) Sum of heights: " << sum << endl;
+    //6) Average of all heights
+    cout << "6) Average: " << sum / SIZE << endl;
     return 0;
 }
