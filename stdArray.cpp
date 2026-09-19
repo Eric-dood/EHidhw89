cef21d483c13fd3846a2f7b4fdd7cb8476076459
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-18 18:22:37 -0700

    Done with std:Array

--- stdArray.cpp
+++ stdArray.cpp
@@ -55,8 +55,7 @@ int main()
     cout << "6) Random element by using at(): " << heights.at(rand() % (SIZE - 1)) << endl;
     //7) Sum of all elements
     int sum;
-    for (int i = 0; i < SIZE; i++)
-        sum += heights[i];
+    for (int i = 0; i < SIZE; i++) sum += heights[i]; //Use a for-loop to add the sum
     cout << "7) Sum of heights: " << sum << endl;
     //8) Average of all heights
     cout << "8) Average: " << sum / SIZE << endl;
@@ -69,25 +68,29 @@ int main()
     cout << "11) Sorted array: ";
     array<int, SIZE> sortArr = heights;
     sort(sortArr.begin(), sortArr.end());
-    printArray(sortArr);
+    printArray(sortArr); //use printArray() to print the reversed array
 
     //12) Reverse the array by reverse()
     cout << "12) Reversed array by reverse(): ";
     array<int, SIZE> reverseArr = heights;
     reverse(reverseArr.begin(), reverseArr.end());
-    printArray(reverseArr);
+    printArray(reverseArr); //use printArray() to print the reversed array
 
     //13) Reverse the array manually
     cout << "13) Manually reversed array: ";
     array<int, SIZE> reverseArr2 = heights;
     for (int i = 0; i < (SIZE / 2); i++)
     {
+        //Use 'last' for the last element
         int last = SIZE - (i + 1);
+        //Define temp as the last element
         int temp = reverseArr2[last];
+        //Turn the last element to the first element's number
         reverseArr2[last] = reverseArr2[i];
+        //The ith element becomes the last element's number
         reverseArr2[i] = temp;
     }
-    printArray(reverseArr2);
+    printArray(reverseArr2); //use printArray() to print the new array
 
     //14) Find address for the array by data()
     cout << "14) Array address by using data(): " << heights.data() << endl;
@@ -104,32 +107,32 @@ int main()
     cout << "19) Reverse sorted array by using rbegin() & rend(): ";
     array<int, SIZE> sortArr2 = heights;
     sort(sortArr2.rbegin(), sortArr2.rend());
-    printArray(sortArr2);
+    printArray(sortArr2); //use printArray() to print the array
 
     //20) Search for an array by using find()
     int target = 157;
     cout << "20) Find for an array by using find(): ";
-    auto t = find(heights.begin(), heights.end(), target);
+    auto t = find(heights.begin(), heights.end(), target); //Use find() to search for the target value
     cout << "Value " << target << " ";
-    if (t != heights.end())
+    if (t != heights.end()) //If the target hasn't reached the end
         cout << "found at position " << (t - heights.begin()) << ".";
-    else
+    else //Otherwise send a 'not found' message.
         cout << "not found.";
     cout << endl;
 
     //21) Create two empty arrays and fill 'em up
     array<int, 5> arr1, arr2;
-    fill(arr1.begin(), arr1.end(), 1);
-    fill(arr2.begin(), arr2.end(), 2);
+    fill(arr1.begin(), arr1.end(), 1); //Fill up array 1 with ones
+    fill(arr2.begin(), arr2.end(), 2); //Fill up array 2 with twos
     cout << "21) Create two arrays and use fill(): " << endl;
     cout << "Array 1: ";
-    for (int i = 0; i < arr1.size(); i++) cout << arr1.at(i) << " ";
+    for (int i = 0; i < arr1.size(); i++) cout << arr1.at(i) << " "; //Print arr1
     cout << endl << "Array 2: ";
-    for (int i = 0; i < arr2.size(); i++) cout << arr2.at(i) << " ";
+    for (int i = 0; i < arr2.size(); i++) cout << arr2.at(i) << " "; //Print arr2
     cout << endl;
 
     //22) Swap two of the arrays
-    swap(arr1, arr2);
+    swap(arr1, arr2); //This function swaps the elements of arr1 and arr2
     cout << "21) Swap the two arrays by using swap(): " << endl;
     cout << "Array 1: ";
     for (int i = 0; i < arr1.size(); i++) cout << arr1.at(i) << " ";
@@ -138,15 +141,14 @@ int main()
     cout << endl;
 
     //Create a 2D array
-    array<int, 3> ar1 = { 1, 2, 3 }, ar2 = { 4, 5, 6 };
-    array<array<int, 3>, 3> arr2D = { ar1, ar2 };
-    for (int i = 0; i < ar1.size(); i++)
+    array<int, 3> ar1 = { 1, 2, 3 }, ar2 = { 4, 5, 6 }; //Create ar1 & ar2 as separate 1D-arrays
+    array<array<int, 3>, 3> arr2D = { ar1, ar2 }; //Then do a 2D-array with both ar1 and ar2
+    for (int i = 0; i < (ar1.size() + ar2.size()); i++)
     {
-        for (int j = 0; j < ar2.size(); j++)
-            cout << ar2[j] << " ";
-        cout << ar1[i] << endl;
+        if (i == ar1.size()) cout << endl; //If the first array has been printed out, leave in space
+        if (i < ar1.size()) cout << ar1[i] << " "; //Print out ar1 elements
+        else cout << ar2[i - ar1.size()] << " "; //Print out ar2 elements
     }
-
 
     return 0;
 }
